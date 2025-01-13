7c7da8240a693b07b7e867641fc9fae79af97a56
chififid <who>
2025-01-13 16:48:01 +0300

    update secretno1337

--- src/experiments/secretno1337/index.js
+++ src/experiments/secretno1337/index.js
@@ -5,8 +5,8 @@ import { run } from "/src/main.js";
 const ro = [
   await getRelativeObject(
     "/src/assets/secretno1337.gltf",
-    new Vector3(0, 0, -8),
-    new Vector3(0, 0, 0),
+    new Vector3(-5 , 0, -8),
+    new Vector3(0.5, 0, -0.2),
     new Vector3(0.3, 0.6, 0),
     new Vector3(1, 1, 1),
     new Vector3(0, 0, 0),
